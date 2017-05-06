c6b12b9f5de93459cb7221f1fe9519d5847e225d
edgardvs234 <edgardvs432@gmail.com>
2017-05-06 18:18:20 -0500

    v2.1
    probando codigo plsql

--- users/views.py
+++ users/views.py
@@ -6,27 +6,13 @@ import json
 def login(request):
 
     data= {'resultado':True}
-    	conn_string = "host='127.0.0.1' dbname='miapp_db' user='miapp' password='miapp'"
-	# print the connection string we will use to connect
-	print "Connecting to database\n	->%s" % (conn_string)
-
-	# get a connection, if a connect cannot be made an exception will be raised here
-	conn = psycopg2.connect(conn_string)
-
-	# conn.cursor will return a cursor object, you can use this cursor to perform queries
-	cursor = conn.cursor()
-
-	# execute our Query
-	cursor.execute("SELECT * FROM Usuario")
-
-	# retrieve the records from the database
-	records = cursor.fetchall()
-
-	# print out the records using pretty print
-	# note that the NAMES of the columns are not shown, instead just indexes.
-	# for most people this isn't very useful so we'll show you how to return
-	# columns as a dictionary (hash) in the next example.
-	pprint.pprint(records)
+    conn_string = "host='127.0.0.1' dbname='miapp_db' user='miapp' password='miapp'"
+    print ("Connecting to database\n	->%s" % (conn_string))
+    conn = psycopg2.connect(conn_string)
+    cursor= conn.cursor()
+    cursor.execute("SELECT * FROM Usuario")
+    usuarios = cursor.fetchall
+    print(usuarios)
     json_data= json.dumps(data)
     print(json_data)
     return HttpResponse(json_data, content_type= 'application/json')
