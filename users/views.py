4918a32efe10f52ede7bcd1041043cc56aa7669e
edgardvs234 <edgardvs432@gmail.com>
2017-05-07 17:21:43 -0500

    mas intentos

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def login(request):
     data= {'resultado':True}
 
 
-    conn = psycopg2.connect("dbname=ddgrh85co1hhsd user=txmdzfeapxbwss password=27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299 host=ec2-174-129-227-116.compute-1.amazonaws.com port=5432")
+    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     cursor= conn.cursor()
     cursor.execute('SELECT id, nombre, usuario, clave FROM public."Usuario";')
 
