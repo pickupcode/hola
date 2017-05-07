0dcd795b7c4fc3679ee3732e25a2f73c04d4c89e
edgardvs234 <edgardvs432@gmail.com>
2017-05-06 22:00:44 -0500

    pureba de conexion

--- users/views.py
+++ users/views.py
@@ -13,8 +13,9 @@ def login(request):
     #print ("Connecting to database\n	->%s" % (conn_string))
     conn = psycopg2.connect("host=ec2-174-129-227-116.compute-1.amazonaws.com dbname=ddgrh85co1hhsd user=txmdzfeapxbwss password=27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299")
     cursor= conn.cursor()
-    cursor.execute("SELECT * FROM Usuario")
-    usuarios = cursor.fetchall
+    #cursor.execute("SELECT * FROM Usuario")
+    print("conecto")
+    #usuarios = cursor.fetchall
     print(usuarios)
     json_data= json.dumps(data)
     print(json_data)
