de58abcc4eda0c5ea2a98be17672cc1cbc159dc1
edgardvs234 <edgardvs432@gmail.com>
2017-05-07 17:17:11 -0500

    sdasd

--- miapp/settings.py
+++ miapp/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'miapp.wsgi.application'
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'miapp_db',
-        'USER': 'miapp',
-        'PASSWORD': 'miapp',
-        'HOST': '127.0.0.1'
+        'NAME': 'ddgrh85co1hhsd',
+        'USER': 'txmdzfeapxbwss',
+        'PASSWORD': '27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299',
+        'HOST': 'ec2-174-129-227-116.compute-1.amazonaws.com'
     }
 }
 
