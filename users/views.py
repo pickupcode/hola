fdcd6ad51ddc720b17807f7960775bb7d83f5443
edgardvs234 <edgardvs432@gmail.com>
2017-05-08 11:44:05 -0500

    poniendo if

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def login(request):
 
     data= {'resultado':True}
     usuariobd= "edvs"
-    password = "abc"
+    clavebd = "abc"
 
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
@@ -29,9 +29,13 @@ def login(request):
         print (row)
         #clave ubicada
         print(usuarios[1][1])
+        #convertir a json el usuario y clave no
         json_usuario= json.dumps(usuarios[1])
         print("el json")
         print(json_usuario)
+        #logica
+        if usuariobd = usuarios[i][0] and clavebd = usuario[i][1]:
+            print("el usuario existe")
 
     #json_usuarios= json.dumps(usuarios)
     #i=1
