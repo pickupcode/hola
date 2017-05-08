586b076ab2acb753a20eec414504d1c8514b1cf7
edgardvs234 <edgardvs432@gmail.com>
2017-05-08 16:31:12 -0500

    empezando el register

--- users/views.py
+++ users/views.py
@@ -87,6 +87,38 @@ def login(request):
 
 def register(request):
 
+    nombrein= "jose"
+    usuarioin= "jc"
+    passwordin= "abc"
+
+    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
+    print('aqui esta la conexion')
+    print(conn)
+    cursor= conn.cursor()
+    #query pinta usuario y clave
+    cursor.execute('SELECT usuario, clave FROM "Usuario"')
+    for row in usuarios:
+        print (row)
+        #usuario ubicado
+        #print("usuario ubicado")
+        print(usuarios[1][0])
+        #clave ubicada
+        #print("clave del usuario ubicado")
+        print(usuarios[1][1])
+
+        #convertir a json el usuario y clave no
+        #json_usuario= json.dumps(usuarios[1])
+        #print("el json")
+        #print(json_usuario)
+        #logica
+        if usuariobd not in usuarios[i][0]:
+            print("clave")
+            print(usuarios[i][1])
+            
+            print("el usuario existe")
+
+
+
     data= {'resultado':False}
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
