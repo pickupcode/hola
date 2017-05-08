749b995704d7e4a668976efa0d0d2b16771bfc34
edgardvs234 <edgardvs432@gmail.com>
2017-05-08 08:57:18 -0500

    v

--- users/views.py
+++ users/views.py
@@ -13,6 +13,7 @@ def login(request):
 
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
+    print(conn)
     cursor= conn.cursor()
     cursor.execute('SELECT usuario FROM "Usuario"')
 
