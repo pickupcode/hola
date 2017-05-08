fbe5da1971de681e01fadc673b91e5790526f360
edgardvs234 <edgardvs432@gmail.com>
2017-05-08 13:44:49 -0500

    logica login lista

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def login(request):
     data= {'resultado':True}
 
     usuariobd= "edvs"
-    clavebd = "abca"
+    clavebd = "abc"
 
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
