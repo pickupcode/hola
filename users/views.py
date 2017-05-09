1cd3b3d25927eaa4323bdcd7c82c4b7c898ef95c
edgardvs234 <edgardvs432@gmail.com>
2017-05-09 16:24:44 -0500

    espacio

--- users/views.py
+++ users/views.py
@@ -93,7 +93,7 @@ def register(request):
     passwordin= 'abc'
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
-     conn.autocommit = True
+    conn.autocommit = True
     print('aqui esta la conexion')
     print(conn)
     cursor= conn.cursor()
