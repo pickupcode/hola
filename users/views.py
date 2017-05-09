9a25f2633cce1ee94581de17b86b0c0791cbee55
edgardvs234 <edgardvs432@gmail.com>
2017-05-09 14:50:43 -0500

    asas

--- users/views.py
+++ users/views.py
@@ -134,7 +134,7 @@ def register(request):
                 cursor2= conn.cursor()
                 data=(nombrein,usuarioin,passwordin)
                 print(data)
-                cursor2.execute('INSERT INTO "Usuario" (nombre, usuario, clave) VALUES (%s, %s, %s)',(nombrein,usuarioin,passwordin))
+                cursor2.execute('INSERT INTO "Usuario" (nombre, usuario, clave) VALUES (%s, %s, %s)',('jose','jc','abc'))
                 print("usuario creado con exito")
 
         i= i+1
