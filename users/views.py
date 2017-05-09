4ee144a9fd4fa058fc5292527e7a6e572afce959
edgardvs234 <edgardvs432@gmail.com>
2017-05-09 17:40:36 -0500

    json tipo user

--- users/views.py
+++ users/views.py
@@ -13,10 +13,14 @@ def login(request):
     #usuarioget=request.GET["username"]
     #claveget=request.GET["password"]
 
-    data= {'resultado':True}
+    #data= {'resultado':True}
 
     usuariobd= "pepe"
     clavebd = "abc"
+    data2={'User':{'usuario': usuariobd, 'clave': clavebd}}
+    json_data2= json.dumps(data2)
+    print("nuevo json")
+    print(json_data2)
 
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
@@ -130,7 +134,7 @@ def register(request):
             j= j+1
             print(j)
 
-            
+
             if j==total:
 
                 print("insertar el usuario")
