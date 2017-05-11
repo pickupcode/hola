e28061b9267eb8b1713f6089f2104571940d5d30
Gonzalo Reyes <gonzalorehu.97.31@gmail.com>
2017-05-11 10:13:51 -0500

    Cambio de logica en el Login

--- users/views.py
+++ users/views.py
@@ -20,10 +20,10 @@ def login(request):
 
     usuariobd= request.GET["username"]
     clavebd = request.GET["password"]
-    data2={'User':{'usuario': usuariobd, 'clave': clavebd}}
-    json_data2= json.dumps(data2)
-    print("nuevo json")
-    print(json_data2)
+    # data2={'User':{'usuario': usuariobd, 'clave': clavebd}}
+    # # json_data2= json.dumps(data2)
+    # print("nuevo json")
+    # print(json_data2)
 
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
@@ -31,62 +31,80 @@ def login(request):
     print(conn)
     cursor= conn.cursor()
     #query pinta usuario y clave
-    cursor.execute('SELECT usuario, clave FROM usuarios')
+    cursor.execute('SELECT usuario, clave FROM usuarios WHERE usuario = %s', usuario)
 
+    # Result Set
+    rs = cursor.fetchall()
+    if rs.rowcount == 0:
+        #Usuario no registrado en la bd
+        data= {'usuario': "", 'clave': ""}
 
-    usuarios = cursor.fetchall()
-    #print(usuarios)
-    i=0
-    for row in usuarios:
-        print (row)
-        #usuario ubicado
-        #print("usuario ubicado")
-        print(usuarios[1][0])
-        #clave ubicada
-        #print("clave del usuario ubicado")
-        print(usuarios[1][1])
-
-        #convertir a json el usuario y clave no
-        #json_usuario= json.dumps(usuarios[1])
-        #print("el json")
-        #print(json_usuario)
-        #logica
-        if usuariobd in usuarios[i][0] and clavebd in usuarios[i][1]:
-            print("clave")
-            print(usuarios[i][1])
-            print("el usuario existe")
-            #data= {'resultado':True}
+    else:
+        #rs.1 es la clave
+        if clavebd == rs[1]:
+            #Usuario y Password Correcto
             data={'usuario': usuariobd, 'clave': clavebd}
-            json_data= json.dumps(data)
-            print(json_data)
-            return HttpResponse(json_data, content_type= 'application/json')
-        elif usuariobd in usuarios[i][0] and clavebd not in usuarios[i][1]:
-            print("La contrasena no es correcta")
-            data= {'usuario': "", 'clave': ""}
-            json_data= json.dumps(data)
-            print(json_data)
-            return HttpResponse(json_data, content_type= 'application/json')
-        elif usuariobd not in usuarios[i][0] and clavebd in usuarios[i][1]:
-            print("usuario no esta registrado")
-            data= {'usuario': "", 'clave': ""}
-            json_data= json.dumps(data)
-            print(json_data)
-            return HttpResponse(json_data, content_type= 'application/json')
-        #elif usuariobd not in usuarios[i][0] and clavebd not in usuarios[i][1]:
-            #print("La contrasena no es correcta")
-            #data= {'resultado':False}
-            #json_data= json.dumps(data)
-            #print(json_data)
-            #return HttpResponse(json_data, content_type= 'application/json')
+
         else:
-            print("informacion incorrecta")
+            #Password incorrecta
             data= {'usuario': "", 'clave': ""}
-            json_data= json.dumps(data)
-            print(json_data)
-            return HttpResponse(json_data, content_type= 'application/json')
-        #puntero
-        i= i+1
-        print (i)
+    json_data= json.dumps(data)
+    print(json_data)       
+    return HttpResponse(json_data, content_type= 'application/json')
+
+    # usuarios = cursor.fetchall()
+    # #print(usuarios)
+    # i=0
+    # for row in usuarios:
+    #     print (row)
+    #     #usuario ubicado
+    #     #print("usuario ubicado")
+    #     print(usuarios[1][0])
+    #     #clave ubicada
+    #     #print("clave del usuario ubicado")
+    #     print(usuarios[1][1])
+    #
+    #     #convertir a json el usuario y clave no
+    #     #json_usuario= json.dumps(usuarios[1])
+    #     #print("el json")
+    #     #print(json_usuario)
+    #     #logica
+    #     if usuariobd in usuarios[i][0] and clavebd in usuarios[i][1]:
+    #         print("clave")
+    #         print(usuarios[i][1])
+    #         print("el usuario existe")
+    #         #data= {'resultado':True}
+    #         data={'usuario': usuariobd, 'clave': clavebd}
+    #         json_data= json.dumps(data)
+    #         print(json_data)
+    #         return HttpResponse(json_data, content_type= 'application/json')
+    #     elif usuariobd in usuarios[i][0] and clavebd not in usuarios[i][1]:
+    #         print("La contrasena no es correcta")
+    #         data= {'usuario': "", 'clave': ""}
+    #         json_data= json.dumps(data)
+    #         print(json_data)
+    #         return HttpResponse(json_data, content_type= 'application/json')
+    #     elif usuariobd not in usuarios[i][0] and clavebd in usuarios[i][1]:
+    #         print("usuario no esta registrado")
+    #         data= {'usuario': "", 'clave': ""}
+    #         json_data= json.dumps(data)
+    #         print(json_data)
+    #         return HttpResponse(json_data, content_type= 'application/json')
+    #     #elif usuariobd not in usuarios[i][0] and clavebd not in usuarios[i][1]:
+    #         #print("La contrasena no es correcta")
+    #         #data= {'resultado':False}
+    #         #json_data= json.dumps(data)
+    #         #print(json_data)
+    #         #return HttpResponse(json_data, content_type= 'application/json')
+    #     else:
+    #         print("informacion incorrecta")
+    #         data= {'usuario': "", 'clave': ""}
+    #         json_data= json.dumps(data)
+    #         print(json_data)
+    #         return HttpResponse(json_data, content_type= 'application/json')
+    #     #puntero
+    #     i= i+1
+    #     print (i)
 
 
 
