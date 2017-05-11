af68d2993713fd84fe676fbbd552a2a0f9e0d385
edgardvs234 <edgardvs432@gmail.com>
2017-05-11 11:22:05 -0500

    register

--- users/views.py
+++ users/views.py
@@ -119,6 +119,8 @@ def user_exists(usuario):
 
 def register(request):
 
+
+
     nombrein= request.GET["name"]
     usuarioin= request.GET["username"]
     passwordin= request.GET["password"]
@@ -126,42 +128,43 @@ def register(request):
     user_exists = user_exists(usuarioin)
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
+    conn.autocommit = True
+    #data= {'resultado':False}
+    #if user_exists == False:
+        #cursor= conn.cursor()
+        #valores = (nombrein, usuarioin, passwordin)
+        #query = 'INSERT INTO usuarios (nombre, usuario, clave) VALUES (%s,%s,%s)' % valores
+        #cursor.execute(query)
+        #conn.commit()
+        #data= {'resultado': True}
 
-    data= {'resultado':False}
-    if user_exists == False:
-        cursor= conn.cursor()
-        valores = (nombrein, usuarioin, passwordin)
-        query = 'INSERT INTO usuarios (nombre, usuario, clave) VALUES (%s,%s,%s)' % valores
-        cursor.execute(query)
-        conn.commit()
-        data= {'resultado': True}
-
-    json_data= json.dumps(data)
-    return HttpResponse(json_data, content_type= 'application/json')
-
-
+    #json_data= json.dumps(data)
+    #return HttpResponse(json_data, content_type= 'application/json')
 
 
 
 
-    # print('aqui esta la conexion')
-    # print(conn)
-    # cursor= conn.cursor()
-    # #query pinta usuario y clave
-    # cursor.execute('SELECT usuario, clave FROM usuarios')
+
+
+    print('aqui esta la conexion')
+    print(conn)
+    cursor= conn.cursor()
+    #query pinta usuario y clave
+    cursor.execute('SELECT usuario, clave FROM usuarios')
+
+    usuarios = cursor.fetchall()
+    i= 0
+    k= 0
+    j= 0
     #
-    # usuarios = cursor.fetchall()
-    # i= 0
-    # k= 0
-    # j= 0
-    #
-    # for row in usuarios:
-    #     print (row)
-    #     #usuario ubicado
-    #     #print("usuario ubicado")
-    #     #print(usuarios[1][0])
-    #     #clave ubicada
-    #     #print("clave del usuario ubicado")
+    for row in usuarios:
+
+        print (row)
+        usuario ubicado
+        print("usuario ubicado")
+        print(usuarios[1][0])
+        #clave ubicada
+        #print("clave del usuario ubicado")
     #     #print(usuarios[1][1])
     #
     #     #convertir a json el usuario y clave no
@@ -169,39 +172,42 @@ def register(request):
     #     #print("el json")
     #     #print(json_usuario)
     #     #logica
-    #     if usuarioin not in usuarios[i][0]:
-    #         print(len(usuarios))
-    #         total= len(usuarios)
-    #         print("usuario revisado")
-    #         print(usuarios[i][0])
-    #         print("clave")
-    #         print(usuarios[i][1])
-    #         print("contador")
-    #         j= j+1
-    #         print(j)
-    #
-    #
-    #         if j==total:
-    #
-    #             print("insertar el usuario")
-    #             cursor2= conn.cursor()
-    #             data=(nombrein,usuarioin,passwordin)
-    #             print(data)
-                # cursor2.execute("""INSERT INTO usuarios (nombre, usuario, clave) VALUES (%s,%s,%s)""",data)
-    #             #p= Usuario(nombre= nombrein, usuario= usuarioin, clave= passwordin)
-    #             #p.save()
-    #             data= {'resultado':True}
-    #             json_data= json.dumps(data)
-    #             return HttpResponse(json_data, content_type= 'application/json')
-    #
-    #             print("usuario creado con exito")
-    #     else:
-    #         print("el usuario ya existe")
-    #         data= {'resultado':False}
-            # json_data= json.dumps(data)
-            # return HttpResponse(json_data, content_type= 'application/json')
-    #
-    #     i= i+1
+        if usuarioin not in usuarios[i][0]:
+
+             print(len(usuarios))
+             total= len(usuarios)
+             print("usuario revisado")
+             print(usuarios[i][0])
+             print("clave")
+             print(usuarios[i][1])
+             print("contador")
+             j= j+1
+             print(j)
+
+
+             if j==total:
+
+
+                 print("insertar el usuario")
+                 cursor2= conn.cursor()
+                 data=(nombrein,usuarioin,passwordin)
+                 print(data)
+                 cursor2.execute("""INSERT INTO usuarios (nombre, usuario, clave) VALUES (%s,%s,%s)""",data)
+                 #p= Usuario(nombre= nombrein, usuario= usuarioin, clave= passwordin)
+                 #p.save()
+                 data= {'resultado':True}
+                 json_data= json.dumps(data)
+                 return HttpResponse(json_data, content_type= 'application/json')
+
+                 print("usuario creado con exito")
+         else:
+
+             print("el usuario ya existe")
+             data= {'resultado':False}
+             json_data= json.dumps(data)
+             return HttpResponse(json_data, content_type= 'application/json')
+
+         i= i+1
 def listar(request):
     print("el usuario ya existe")
     data= {'categorias':[]}
