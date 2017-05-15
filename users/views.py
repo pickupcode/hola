160d51cd496e95a724005b8d6cf595de638bdd22
Gonzalo Reyes <gonzalorehu.97.31@gmail.com>
2017-05-15 10:20:24 -0500

    error

--- users/views.py
+++ users/views.py
@@ -31,7 +31,7 @@ def login(request):
             clave = rs[0][2]
             data = {'nombre': nombre,'usuario': usuario, 'clave': clave}
 
-    ßjson_data= json.dumps(data)
+    json_data= json.dumps(data)
     print(json_data)
     return HttpResponse(json_data, content_type= 'application/json')
 
