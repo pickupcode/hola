57cf45eec9be75e8dde7cce2170927bc59b460e3
pickupcode <giovanni.pattarone@gmail.com>
2017-05-15 09:49:16 -0500

    Listar perdidos terminado

--- users/views.py
+++ users/views.py
@@ -11,14 +11,14 @@ import pprint
 def login(request):
     usuariobd= request.GET["username"]
     clavebd = request.GET["password"]
-    
+
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
-    
+
     cursor= conn.cursor()
     #query pinta usuario y clave
     query = "SELECT nombre, usuario, clave FROM usuarios WHERE usuario = '%s'" % usuariobd
     cursor.execute(query)
-    
+
     # Result Set
     rs = cursor.fetchall()
     data= {'nombre' : "", 'usuario' : "", 'clave' : ""}
@@ -40,20 +40,20 @@ def user_exists(usuario):
     cursor= conn.cursor()
     query = "SELECT usuario FROM usuarios WHERE usuario = '%s'"  % usuario
     cursor.execute(query)
-    
+
     # Result Set
     rs = cursor.fetchall()
     user_does_exist = len(rs) == 1
     return True if user_does_exist else False
 
 def register(request):
-    
+
     nombrein= request.GET["name"]
     usuarioin= request.GET["username"]
     passwordin= request.GET["password"]
-    
+
     user_does_exist = user_exists(usuarioin)
-    
+
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     data= {'resultado':False}
     if user_does_exist == False:
@@ -65,14 +65,33 @@ def register(request):
         data= {'resultado': True}
         json_data= json.dumps(data)
         return HttpResponse(json_data, content_type= 'application/json')
-    
+
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
 
 def listar(request):
-    print("el usuario ya existe")
-    data= {'categorias':[]}
-    json_data= json.dumps(data)
+    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
+    cursor= conn.cursor()
+    cursor2 = conn.cursor()
+    #query pinta usuario y clave
+    query = "SELECT id, nombre FROM \"Categoria\""
+    query2 = "SELECT dni, firstname, edad, descripcion, lastname, categoria FROM \"perdidos\""
+    cursor.execute(query)
+    cursor2.execute(query2)
+    # Result Set
+    rs = cursor.fetchall()
+    rs2 = cursor2.fetchall()
+    data= [{'categorias':[]}]
+    dato = ""
+    count = 0
+    for e in rs:
+        data[0]['categorias'].append({'perdidos':[],'nombre': e[1]})
+        for i in rs2:
+            if e[0]==i[5]:
+                data[0]['categorias'][count]['perdidos'].append({'dni': i[0],'nombre': i[1],'age': i[2],'description': i[3],'apellido': i[4]})
+        count = count + 1
+    data1 = data[0]
+    json_data= json.dumps(data1)
     return HttpResponse(json_data, content_type= 'application/json')
 
 
