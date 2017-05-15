972d81abc541d8d0952af56e394d211226c36a65
Gonzalo Reyes <gonzalorehu.97.31@gmail.com>
2017-05-15 15:11:38 -0500

    Cambios

--- users/views.py
+++ users/views.py
@@ -79,12 +79,12 @@ def listar(request):
     rs = cursor.fetchall()
     data = {'categorias':[]}
     categoria = {'nombre' : "", 'perdidos' : []}
-    current_category_index = 0
+    current_category_index = -1
     for result in rs:
-        current_category = rs[current_category_index][0]
-        previous_category = rs[current_category_index-1][0] if current_category_index > 0 else ""
+        current_category = rs[current_category_index+1][0]
+        previous_category = rs[current_category_index][0] if current_category_index > 0 else ""
         if current_category != previous_category:
-            current_category_index += 1 if current_category_index > 0 else 0
+            current_category_index += 1
             categoria = {'nombre' : result[1], 'perdidos' : []}
             data['categorias'].append(categoria)
 
