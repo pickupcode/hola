07219b85f5a7e87b83bc7c0e9d6308649f0bdd78
Gonzalo Reyes <gonzalorehu.97.31@gmail.com>
2017-05-15 14:59:24 -0500

    Reducción en el número de conexiones

--- users/views.py
+++ users/views.py
@@ -72,33 +72,6 @@ def register(request):
 def listar(request):
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     cursor= conn.cursor()
-    cursor2 = conn.cursor()
-    #query pinta usuario y clave
-    query = "SELECT id, nombre FROM \"Categoria\""
-    query2 = "SELECT dni, firstname, edad, descripcion, lastname, categoria FROM \"perdidos\""
-    cursor.execute(query)
-    cursor2.execute(query2)
-    # Result Set
-    rs = cursor.fetchall()
-    rs2 = cursor2.fetchall()
-    data= [{'categorias':[]}]
-    dato = ""
-    count = 0
-    for e in rs:
-        data[0]['categorias'].append({'perdidos':[],'nombre': e[1]})
-        for i in rs2:
-            if e[0]==i[5]:
-                data[0]['categorias'][count]['perdidos'].append({'dni': i[0],'nombre': i[1],'age': i[2],'description': i[3],'apellido': i[4]})
-        count = count + 1
-    data1 = data[0]
-    json_data= json.dumps(data1)
-    return HttpResponse(json_data, content_type= 'application/json')
-
-
-
-def test(request):
-    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
-    cursor= conn.cursor()
     #query pinta usuario y clave
     query = "SELECT c.id, c.nombre, p.dni, p.firstname, p.edad, p.descripcion, p.lastname, p.categoria FROM \"Categoria\" as c join \"perdidos\" as p on c.id = p.categoria order by c.id"
     cursor.execute(query)
@@ -118,6 +91,13 @@ def test(request):
         perdido = {'nombre' : result[3], 'apellido' : result[6], 'dni' : result[2], 'age' : result[4], 'description' : result[5]}
         data['categorias'][current_category_index]['perdidos'].append(perdido)
 
+    json_data= json.dumps(data)
+    return HttpResponse(json_data, content_type= 'application/json')
+
+
+
+def test(request):
+    data = {'test' : "Ay Lmao"}    
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
 
