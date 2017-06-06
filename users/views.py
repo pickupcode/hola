25ee7fa8c606be0ebdaabef4bc95b396c1472dfc
edgardvs234 <edgardvs432@gmail.com>
2017-06-06 10:46:25 -0500

    pruebas

--- users/views.py
+++ users/views.py
@@ -98,6 +98,10 @@ def listar(request):
 
 def clue(request):
     print ("entro a clues2")
+    iduser = request.GET["idUsuario"]
+    idlost = request.GET["idPerdido"]
+    asunto = request.GET["asunto"]
+    descripcion = request.GET["descripcion"]
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     data= {'resultado':False}
     cursor= conn.cursor()
