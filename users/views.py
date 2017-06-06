c27a164474047f94cc610c9e260097e5a8f53f6f
greyesh <gonzalorehu.97.31@gmail.com>
2017-06-06 10:57:42 -0500

    Conflictos corregidos

--- users/views.py
+++ users/views.py
@@ -97,33 +97,17 @@ def listar(request):
     return HttpResponse(json_data, content_type= 'application/json')
 
 def clue(request):
-<<<<<<< HEAD
-    print ("entro a clues2")
-    iduser = request.GET["idUsuario"]
-    idlost = request.GET["idPerdido"]
-    asunto = request.GET["asunto"]
-    descripcion = request.GET["descripcion"]
-    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
-    data= {'resultado':False}
-    cursor= conn.cursor()
-    valores = (iduser, idlost, asunto,descripcion)
-    query = "INSERT INTO pista (idusuario, idperdido, asunto, descripcion) VALUES ('%s','%s','%s','%s')" % valores
-
-
-    if descripcion != descripcion.empty or  asunto != asunto.empty:
-=======
     idUsuario = request.GET["idUsuario"]
     idPerdido = request.GET["idPerdido"]
     asunto = request.GET["asunto"]
     descripcion = request.GET["descripcion"]
     data= {'resultado':False}
+    conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
 
     if descripcion != "" and  asunto != "" and len(descripcion) <= 100 and len(asunto) <= 30:
         valores = (idUsuario, idPerdido, asunto,descripcion)
-        conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
         cursor= conn.cursor()
         query = "INSERT INTO pista (idusuario, idperdido, asunto, descripcion) VALUES ('%s','%s','%s','%s')" % valores
->>>>>>> origin/develop
         cursor.execute(query)
         conn.commit()
         data= {'resultado': True}
