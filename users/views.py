45eb6079ba7af23dd6733119e7e8223abe1e87af
edgardvs234 <edgardvs432@gmail.com>
2017-06-06 09:34:05 -0500

    realizando pruebas

--- users/views.py
+++ users/views.py
@@ -97,10 +97,14 @@ def listar(request):
     return HttpResponse(json_data, content_type= 'application/json')
 
 def clue(request):
-    idUsuario = request.GET["idUsuario"]
-    idPerdido = request.GET["idPerdido"]
-    asunto = request.GET["asunto"]
-    descripcion = request.GET["descripcion"]
+    #idUsuario = request.GET["idUsuario"]
+    #idPerdido = request.GET["idPerdido"]
+    #asunto = request.GET["asunto"]
+    #descripcion = request.GET["descripcion"]
+    idUsuario = 1
+    idPerdido = 1
+    asunto = "Persona vista nuevamente"
+    descripcion = "la persona fue encontrada en la calle ..."
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     data= {'resultado':False}
