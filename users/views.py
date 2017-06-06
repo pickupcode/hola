dff150b56c08b2e5f35fed713299fab861eef20d
edgardvs234 <edgardvs432@gmail.com>
2017-06-06 09:58:14 -0500

    error

--- users/views.py
+++ users/views.py
@@ -102,10 +102,7 @@ def clue(request):
     #asunto = request.GET["asunto"]
     #descripcion = request.GET["descripcion"]
     print ("entro a clues")
-    idUsuario = 1
-    idPerdido = 1
-    asunto = "Persona vista nuevamente"
-    descripcion = "la persona fue encontrada en la calle ..."
+    
 
     conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
     data= {'resultado':False}
