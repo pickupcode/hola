3983e381b21ea67be62a1c47aed98c5bea3f3c4e
edgardvs234 <edgardvs432@gmail.com>
2017-06-17 13:55:00 -0500

    registrando modelos restante usuario ya esta

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from users.models import Usuarios
-
+from users.models import Categoria
+from users.models import Perdidos
+from users.models import Pista
 # Register your models here.
 admin.site.register(Usuarios)
+admin.site.register(Pista)
+admin.site.register(Categoria)
+admin.site.register(Perdidos)
