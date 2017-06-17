a847904c7aad40b14d4e997b7e67430a6788f872
edgardvs234 <edgardvs432@gmail.com>
2017-06-17 14:27:34 -0500

    modelos funcionando ya se pueden hacer querys django!!!!!

--- users/views.py
+++ users/views.py
@@ -122,7 +122,7 @@ def clue(request):
 
 def test(request):
     lista_usuarios= Usuarios.objects.all()
-
+    #ya se pueden hacer querys con django!!!!!!!!!!!!!!
     json_estudiantes= serializers.serialize('json',lista_usuarios)
     print(json_estudiantes)
     data = {'test' : "Ay Lmao ay lmao sdad2"}
