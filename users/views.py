4d35dde0fdb85f980fc6def0760408bebf356ffd
edgardvs234 <edgardvs432@gmail.com>
2017-06-17 14:54:09 -0500

    usuario ubicado

--- users/views.py
+++ users/views.py
@@ -126,7 +126,7 @@ def test(request):
     json_estudiantes= serializers.serialize('json',lista_usuarios)
     print(json_estudiantes)
     for usuario in lista_usuarios:
-        print(usuario.nombre)
+        print(usuario.usuario)
     data = {'test' : "Ay Lmao ay lmao sdad2"}
     json_data= json.dumps(data)
     return HttpResponse(json_estudiantes, content_type= 'application/json')
