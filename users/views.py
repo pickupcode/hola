379349913812caad8f5d527c3fa8cc8366607b9a
edgardvs234 <edgardvs432@gmail.com>
2017-06-17 13:06:53 -0500

    realizando migracion

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from users.models import Usuario
+#from users.models import Usuario
 from django.core import serializers
 
 import json
