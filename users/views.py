4ea911db56ebe84a5960648c3ae8d2895b054f66
edgardvs234 <edgardvs432@gmail.com>
2017-06-18 18:34:09 -0500

    pruebas

--- users/views.py
+++ users/views.py
@@ -155,7 +155,7 @@ def test(request):
     #armando el json listar
     lista_perdido= Perdidos.objects.select_related()
     for perdido in lista_perdido:
-        print(perdido.categoria)
+        print(perdido.nombre)
 
     data = {'test' : "Ay Lmao ay lmao sdad2"}
     json_data= json.dumps(data)
