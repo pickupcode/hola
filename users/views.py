4d267ed14849d1a63c277d10a3e8296d70aaa152
edgardvs234 <edgardvs432@gmail.com>
2017-06-20 17:13:00 -0500

    a

--- users/views.py
+++ users/views.py
@@ -179,7 +179,7 @@ def test(request):
             data['categorias'].append(categoria)
             perdido= {'nombre': perdido.firstname, 'apellido': perdido.lastname, 'dni': perdido.dni, 'age': perdido.edad, 'description': perdido.descripcion, 'imagen': perdido.imagen}
             #print(perdido)
-            #print(data['categorias'][i]['perdidos'])
+            print(data['categorias'][0]['perdidos'])
             data['categorias'][i]['perdidos'].append(perdido)
             i= i+1
             j= j+1
