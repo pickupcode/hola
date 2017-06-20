50513dd494b96a89f232bb6a65d6e4fc9f8aaa83
edgardvs234 <edgardvs432@gmail.com>
2017-06-20 14:47:02 -0500

    pruebas

--- users/views.py
+++ users/views.py
@@ -170,9 +170,9 @@ def test(request):
         pk_categoria= categ.id
         print(pk_categoria)
         lista_perdido= Perdidos.objects.filter(id=pk_categoria).order_by('categoria')
-        for perdido in lista_perdido.iterator():
-            categoria= {'nombre': categ.nombre, 'perdidos': []}
-            data['categorias'].append(categoria)
+        #for perdido in lista_perdido.iterator():
+            #categoria= {'nombre': categ.nombre, 'perdidos': []}
+            #data['categorias'].append(categoria)
     print(data)
 
 
