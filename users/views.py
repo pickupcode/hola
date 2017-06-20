b70dd7e09faf66468a947e5c164ef5f8bdbff61d
edgardvs234 <edgardvs432@gmail.com>
2017-06-20 16:51:13 -0500

    cambiando estructura

--- users/views.py
+++ users/views.py
@@ -179,7 +179,7 @@ def test(request):
             perdido= {'nombre': perdido.firstname, 'apellido': perdido.lastname, 'dni': perdido.dni, 'age': perdido.edad, 'description': perdido.descripcion, 'imagen': perdido.imagen}
             #print(perdido)
             #print(data['categorias'][i]['perdidos'])
-            data['categorias'][i]['perdidos'].append(perdido)
+            data['categorias']['perdidos'].append(perdido)
             i= i+1
     print(data)
     json_categoriasxperdidos= json.dumps(data)
