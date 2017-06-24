65ad89f6494cc0fce8635c2cac1ab570e47f8988
edgardvs234 <edgardvs432@gmail.com>
2017-06-24 12:20:58 -0500

    Revert "sadsdasdsdda"
    This reverts commit cd06210986bda70bb8207f69492a675c0f7f1262.

--- users/views.py
+++ users/views.py
@@ -178,15 +178,28 @@ def clue(request):
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
 def report(request):
-
+    usuarioid = request.GET.get("Usuarioid",False)
+    perdidoid = request.GET.get("Perdidoid",False)
+    dni = request.GET("DNI",False)
+    nombre = request.GET.get("Nombre",False)
+    detalle = request.GET.get("Detalle",False)
     data= {'resultado':False}
     lista_denuncias= Denuncia.objects.all()
     json_denuncias= serializers.serialize('json',lista_denuncias)
     print(json_denuncias)
     #conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
 
-
-        
+    if detalle != "" and  dni != "" and len(detalle) <= 600 and len(Dni) <= 20:
+        #valores = (idUsuario, idPerdido, asunto,descripcion)
+        #cursor= conn.cursor()
+        #query = "INSERT INTO pista (idusuario, idperdido, asunto, descripcion) VALUES ('%s','%s','%s','%s')" % valores
+        #cursor.execute(query)
+        #conn.commit()
+        p= Denuncia(idusuario=usuarioid, idperdido= perdidoid, dni= dni, detalle=detalle,nombre=nombre)
+        p.save()
+        data= {'resultado': True}
+        json_data= json.dumps(data)
+        return HttpResponse(json_data, content_type= 'application/json')
 
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
