3c49b181278910dfe7d54ab548323c69d78a0b5f
edgardvs234 <edgardvs432@gmail.com>
2017-06-24 11:50:55 -0500

    probando report

--- users/views.py
+++ users/views.py
@@ -184,19 +184,22 @@ def report(request):
     nombre = request.GET["Nombre"]
     detalle = request.GET["Detalle"]
     data= {'resultado':False}
+    lista_denuncias= Denuncia.objects.all()
+    json_denuncias= serializers.serialize('json',lista_denuncias)
+    print(json_denuncias)
     #conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
 
-    if detalle != "" and  dni != "" and len(detalle) <= 600 and len(Dni) <= 20:
+    #if detalle != "" and  dni != "" and len(detalle) <= 600 and len(Dni) <= 20:
         #valores = (idUsuario, idPerdido, asunto,descripcion)
         #cursor= conn.cursor()
         #query = "INSERT INTO pista (idusuario, idperdido, asunto, descripcion) VALUES ('%s','%s','%s','%s')" % valores
         #cursor.execute(query)
         #conn.commit()
-        p= Denuncia(idusuario=idusuario, idperdido= idperdido, dni= dni, detalle=detalle,nombre=nombre)
-        p.save()
-        data= {'resultado': True}
-        json_data= json.dumps(data)
-        return HttpResponse(json_data, content_type= 'application/json')
+        #p= Denuncia(idusuario=idusuario, idperdido= idperdido, dni= dni, detalle=detalle,nombre=nombre)
+        #p.save()
+        #data= {'resultado': True}
+        #json_data= json.dumps(data)
+        #return HttpResponse(json_data, content_type= 'application/json')
 
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
