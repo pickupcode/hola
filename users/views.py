b1c4a2819ee01f8884d0617af37840b9d6ce47fa
edgardvs234 <edgardvs432@gmail.com>
2017-06-24 12:27:24 -0500

    dni

--- users/views.py
+++ users/views.py
@@ -184,10 +184,10 @@ def report(request):
     nombre = request.GET["Nombre"]
     detalle = request.GET["Detalle"]
     data= {'resultado':False}
-    
+
     #conn = psycopg2.connect("dbname='ddgrh85co1hhsd' user='txmdzfeapxbwss' password='27fd84a2984d45a8416526ce6c1dae1985e8a2de97970fcf21739e79106e6299' host='ec2-174-129-227-116.compute-1.amazonaws.com' port='5432'")
 
-    if detalle != "" and  dni != "" and len(detalle) <= 600 and len(Dni) <= 20:
+    if detalle != "" and  dni != "" and len(detalle) <= 600 and len(dni) <= 20:
         #valores = (idUsuario, idPerdido, asunto,descripcion)
         #cursor= conn.cursor()
         #query = "INSERT INTO pista (idusuario, idperdido, asunto, descripcion) VALUES ('%s','%s','%s','%s')" % valores
