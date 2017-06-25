782809613178e5ffdca3895b7d101e677eb83688
greyesh <gonzalorehu.97.31@gmail.com>
2017-06-24 20:14:57 -0500

    Cambios en cabezera https

--- users/views.py
+++ users/views.py
@@ -28,7 +28,7 @@ def login(request):
                 data = {'name': nombre,'username': usuario}
     json_data= json.dumps(data)
     response = HttpResponse(json_data, content_type= 'application/json')
-    response.__setitem__("Keep-Alive","timeout=5, max=100")
+    response.__delitem__("Connection")
     return response
 
 def user_exists(usuario):
