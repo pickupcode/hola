e324d90e5b19218fc0f63c91c24899436eedb2fa
greyesh <gonzalorehu.97.31@gmail.com>
2017-06-25 15:30:48 -0500

    POST y GET metodos listos

--- users/views.py
+++ users/views.py
@@ -45,15 +45,9 @@ def register(request):
         user.save()
         data= {'result': True}
     else:
-        data = {'result':False}
+        data = {'result': False}
     json_data= json.dumps(data)
     return HttpResponse(json_data, content_type= 'application/json')
-
-def category_matches(cat_a, cat_b):
-    return True if cat_a == cat_b else False
-
-def in_range(index, length):
-    return True if missing_index < length else False
 
 @csrf_exempt
 def list(request):
@@ -79,9 +73,9 @@ def clue(request):
     dni_missing = data['idLostPerson']
     subject = data['subject']
     clue = data['description']
-    data= {'result':False}
+    data= {'result': False}
     if clue != "" and  subject != "" and len(clue) <= 400 and len(subject) <= 30:
-        p= Pista(idusuario = username, idperdido = dni_missing, asunto = subject, descripcion = clue)
+        p = Pista(idusuario = username, idperdido = dni_missing, asunto = subject, descripcion = clue)
         p.save()
         data= {'result': True}
     json_data= json.dumps(data)
@@ -89,13 +83,14 @@ def clue(request):
 
 @csrf_exempt
 def report(request):
-    idusuario = request.POST.get("idUser")
-    idperdido = request.POST.get("idLostPerson",None)
-    nombre = request.POST.get("name",None)
-    detalle = request.POST.get("report")
+    data = json.loads(request.body)
+    username = data['idUser']
+    dni_missing = data.get("idLostPerson", None)
+    name_missing = data.get("name", None)
+    report = data['report']
     data= {'result':False}
-    if detalle != ""  and len(detalle) <= 600:
-        p= Denuncia(idusuario=idusuario, idperdido= idperdido, detalle=detalle,nombre=nombre)
+    if report != ""  and len(report) <= 600:
+        p = Denuncia(idusuario = username, idperdido = dni_missing, detalle = report, nombre = name_missing)
         p.save()
         data= {'result': True}
     json_data= json.dumps(data)
