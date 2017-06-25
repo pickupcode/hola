99fd1bf744aca7f6fe9b61424963f9046f251572
greyesh <gonzalorehu.97.31@gmail.com>
2017-06-24 19:33:02 -0500

    Cambiar status de conexión en cabecera http

--- users/views.py
+++ users/views.py
@@ -27,8 +27,8 @@ def login(request):
                 clave = i.clave
                 data = {'name': nombre,'username': usuario}
     json_data= json.dumps(data)
-    header = {}
-    response = HttpResponse(json_data, status = 201, content_type= 'application/json')
+    header = {"Connection":"close"}
+    response = HttpResponse(json_data, headers = header, content_type= 'application/json')
     return response
 
 def user_exists(usuario):
